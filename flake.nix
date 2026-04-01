9bcadb2119b5c697116345500a1b2c9de1590770
Selim Bucher <selim.bucher@slimmer.ch>
2026-04-02 00:11:36 +0200

    chore: we don't need to prestart the daemon

--- flake.nix
+++ flake.nix
@@ -162,9 +162,6 @@ CSSEOF
         ${ni-setup}/bin/ni-setup --ui
       fi
 
-      if ! pgrep -f "NTKDaemon.exe" > /dev/null 2>&1; then
-        ${wine}/bin/wine "$NTK_EXE"
-      fi
       ${wine}/bin/wine "$WINEPREFIX/drive_c/users/$USER/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Native Access.lnk"
     '';
 
